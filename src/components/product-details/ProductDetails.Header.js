eaa7307b8ee8f1bb0b58c7615bab3fc0271d8486
Sergey Konakov <r3dblr@gmail.com>
2018-12-12 01:13:53 +0300

    Custom header for Details screen.

---
+++ src/components/product-details/ProductDetails.Header.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { Image, Text, View } from 'react-native';
+// styles
+import Styles from './ProductDetails.Styles';
+
+export default class Header extends Component {
+    render() {
+        const { icon, name } = this.props;
+
+        return (
+            <View style={Styles.titleContainer}>
+                <Image style={Styles.icon} source={icon} />
+                <View style={Styles.flexContainer}>
+                    <Text style={Styles.title} numberOfLines={1}>{name}</Text>
+                </View>
+            </View>
+        );
+    }
+};
