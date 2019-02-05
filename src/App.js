36340364c24f33a3187bb252183ecaa44ba72259
Sergey Konakov <r3dblr@gmail.com>
2019-02-06 00:06:40 +0300

    Add splash screen.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import SplashScreen from 'react-native-splash-screen';
 import Navigator from './routes/Routes';
 
 const AppContainer = createAppContainer(Navigator);
 
 export default class App extends React.Component {
+    componentDidMount() {
+        SplashScreen.hide();
+    }
+
     render() {
         return <AppContainer />;
     }
